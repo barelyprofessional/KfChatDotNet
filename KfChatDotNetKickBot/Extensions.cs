bdabe29b83628cc551b95faa10509e2a6d76fdd4
barelyprofessional <150058423+barelyprofessional@users.noreply.github.com>
2024-03-26 16:35:45 +0800

    Fix Kick emote regex so it's less greedy and can handle consecutive emotes with no space

--- KfChatDotNetKickBot/Extensions.cs
+++ KfChatDotNetKickBot/Extensions.cs
@@ -7,7 +7,7 @@ public static class Extensions
     /// Emotes are encoded like [emote:161238:russW] which translates to -> https://files.kick.com/emotes/161238/fullsize
     public static string TranslateKickEmotes(this string s)
     {
-        Regex regex = new Regex(@"\[(.+?):(\d+):(\S+)\]");
+        Regex regex = new Regex(@"\[(.+?):(\d+):(\S+?)\]");
         var matches = regex.Matches(s);
         if (matches.Count == 0)
         {
