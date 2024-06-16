5cdab275c352119377641ace62bd26303080c9b3
barelyprofessional <150058423+barelyprofessional@users.noreply.github.com>
2024-06-16 12:18:56 +0800

    3xpl websocket client in case anyone wanted one. Don't bother using it though, their websocket service is a piece of shit that's totally broken which I only found out after wasting a day on it.

---
+++ ThreeXplCliClient/ThreeXplClient.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json;
+using Spectre.Console;
+using ThreeXplWsClient.Events;
+
+namespace ThreeXplCliClient;
+
+public class ThreeXplClient
+{
+    private List<string> _addresses =
+    [
+        "MC8TiBEsnQVjxbvLtTsUXjTBZTQaR8fe8X",
+        "ltc1qks2m7hvmhs3c20zrfvptv9pvk82p8g70sgw5mk"
+    ];
+    public async Task Start()
+    {
+        var client = new ThreeXplWsClient.ThreeXplWsClient();
+        client.OnThreeXplPush += OnThreeXplEvent;
+        await client.StartWsClient();
+        while (true)
+        {
+            var prompt = AnsiConsole.Ask<string>("Channel: ");
+            client.SendSubscribeRequest(prompt);
+        }
+    }
+
+    private void OnThreeXplEvent(object sender, ThreeXplPushModel e, int connectionId)
+    {
+        AnsiConsole.MarkupLine("[blue]Received event from 3xpl[/]");
+        foreach (var txn in e.Pub.Data.Data)
+        {
+            if (txn.Address == null) return;
+            if (_addresses.Contains(txn.Address))
+            {
+                AnsiConsole.MarkupLine($"[green]Saw txn I'm interested in: {txn.Address}, effect {txn.Effect}, currency {txn.Currency}[/]");
+            }
+        }
+    }
+}
---
+++ ThreeXplWsClient/Events/EventModels.cs
@@ -0,0 +1,110 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ThreeXplWsClient.Events;
+
+public class BaseThreeXplPacketModel
+{
+    [JsonPropertyName("connect")]
+    public ThreeXplConnectDataModel? Connect { get; set; }
+    [JsonPropertyName("id")]
+    public int? Id { get; set; }
+    [JsonPropertyName("error")]
+    public ThreeXplErrorModel? Error { get; set; }
+    [JsonPropertyName("subscribe")]
+    public ThreeXplSubscribeModel? Subscribe { get; set; }
+    [JsonPropertyName("push")]
+    public ThreeXplPushModel? Push { get; set; }
+    
+}
+
+public class ThreeXplDataModel
+{
+    [JsonPropertyName("blockchain")]
+    public string? Blockchain { get; set; }
+    [JsonPropertyName("module")]
+    public string? Module { get; set; }
+    [JsonPropertyName("block")]
+    public int? Block { get; set; }
+    [JsonPropertyName("transaction")]
+    public string? Transaction { get; set; }
+    [JsonPropertyName("sort_key")]
+    public int? SortKey { get; set; }
+    [JsonPropertyName("time")]
+    public DateTimeOffset? Time { get; set; }
+    [JsonPropertyName("currency")]
+    public string? Currency { get; set; }
+    [JsonPropertyName("effect")]
+    public string? Effect { get; set; }
+    [JsonPropertyName("failed")]
+    public bool? Failed { get; set; }
+    [JsonPropertyName("extra")]
+    public object? Extra { get; set; }
+    [JsonPropertyName("extra_indexed")]
+    public object? ExtraIndexed { get; set; }
+    [JsonPropertyName("address")]
+    public string? Address { get; set; }
+}
+
+public class ThreeXplContextModel
+{
+    // "time":"0.21778600 1718465848"
+    [JsonPropertyName("time")]
+    public string? Time { get; set; }
+}
+
+public class ThreeXplConnectDataModel
+{
+    [JsonPropertyName("client")]
+    public string? Client { get; set; }
+    [JsonPropertyName("version")]
+    public string? Version { get; set; }
+    [JsonPropertyName("ping")]
+    public int? Ping { get; set; }
+    [JsonPropertyName("pong")]
+    public bool? Pong { get; set; }
+}
+
+public class ThreeXplErrorModel
+{
+    [JsonPropertyName("code")]
+    public int? Code { get; set; }
+    [JsonPropertyName("Message")]
+    public string? Message { get; set; }
+    [JsonPropertyName("temporary")]
+    public bool? Temporary { get; set; }
+}
+
+public class ThreeXplSubscribeModel
+{
+    [JsonPropertyName("recoverable")]
+    public bool? Recoverable { get; set; }
+    [JsonPropertyName("epoch")]
+    public string? Epoch { get; set; }
+    [JsonPropertyName("positioned")]
+    public bool? Positioned { get; set; }
+}
+
+public class ThreeXplPushModel
+{
+    [JsonPropertyName("channel")]
+    public required string Channel { get; set; }
+    [JsonPropertyName("pub")]
+    public required ThreeXplPushPubModel Pub { get; set; }
+    [JsonPropertyName("offset")]
+    public int? Offset { get; set; }
+    
+}
+
+public class ThreeXplPushPubModel
+{
+    [JsonPropertyName("data")]
+    public required ThreeXplPushDataModel Data { get; set; }
+}
+
+public class ThreeXplPushDataModel
+{
+    [JsonPropertyName("data")]
+    public required List<ThreeXplDataModel> Data { get; set; }
+    [JsonPropertyName("context")]
+    public required ThreeXplContextModel Context { get; set; }
+}
