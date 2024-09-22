88b8f20981caf7e0dfa18d31e248869d39aa837b
barelyprofessional <150058423+barelyprofessional@users.noreply.github.com>
2024-09-22 13:28:49 +0800

    Converted to async main method

--- KfChatDotNetBot/Program.cs
+++ KfChatDotNetBot/Program.cs
@@ -7,16 +7,16 @@ namespace KfChatDotNetBot
 {
     public class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             var logger = LogManager.GetCurrentClassLogger();
             logger.Info("Opening up DB to perform a migration (if one is needed)");
-            using var db = new ApplicationDbContext();
-            db.Database.Migrate();
+            await using var db = new ApplicationDbContext();
+            await db.Database.MigrateAsync();
             logger.Info("Migration done. Syncing builtin settings keys");
-            BuiltIn.SyncSettingsWithDb().Wait();
+            await BuiltIn.SyncSettingsWithDb();
             logger.Info("Migrating settings from config.json (if needed)");
-            BuiltIn.MigrateJsonSettingsToDb().Wait();
+            await BuiltIn.MigrateJsonSettingsToDb();
             logger.Info("Handing over to bot now");
             Console.OutputEncoding = Encoding.UTF8;
             new ChatBot();
