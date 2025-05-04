f68f094b2abd9bc889d888a8ca6843ad15a5706f
barelyprofessional <150058423+barelyprofessional@users.noreply.github.com>
2025-05-03 23:16:07 -0500

    Forgot to add original property names to model

--- KfChatDotNetBot/Models/BetBoltModels.cs
+++ KfChatDotNetBot/Models/BetBoltModels.cs
@@ -1,24 +1,41 @@
-﻿namespace KfChatDotNetBot.Models;
+﻿using System.Text.Json.Serialization;
+
+namespace KfChatDotNetBot.Models;
 
 public class BetBoltBetPayloadModel
 {
     // I've always seen this sent but never know if it'll be null for privacy at some point
+    [JsonPropertyName("username")]
     public string? Username { get; set; }
+    [JsonPropertyName("game_code")]
     public string? GameCode { get; set; }
+    [JsonPropertyName("game_name")]
     public required string GameName { get; set; }
+    [JsonPropertyName("rank")]
     public string? Rank { get; set; }
+    [JsonPropertyName("time")]
     public required DateTimeOffset Time { get; set; }
+    [JsonPropertyName("crypto_code")]
     public required string CryptoCode { get; set; }
+    [JsonPropertyName("bet_amount_fiat")]
     public required string BetAmountFiat { get; set; }
+    [JsonPropertyName("bet_amount_crypto")]
     public required string BetAmountCrypto { get; set; }
+    [JsonPropertyName("win_amount_fiat")]
     // Negatives for losses
     public required string WinAmountFiat { get; set; }
+    [JsonPropertyName("win_amount_crypto")]
     public required string WinAmountCrypto { get; set; }
+    [JsonPropertyName("multiplier")]
     // null on losses
     public string? Multiplier { get; set; }
+    [JsonPropertyName("category_icon")]
     public string? CategoryIcon { get; set; }
+    [JsonPropertyName("types")]
     public List<string>? Types { get; set; }
+    [JsonPropertyName("type")]
     public string? Type { get; set; }
+    [JsonPropertyName("topic")]
     public required string Topic { get; set; }
 }
 
