7ffeb0b562675b34ea0969e8400444e6a715ec6c
alogindtractor <251821224+A-Log-In-D-Tractor@users.noreply.github.com>
2026-02-08 11:08:12 -0800

    Optimize message retrieval in MinesCommand
    Refactor message handling in MinesCommand to use last message directly.

--- KfChatDotNetBot/Commands/Kasino/MinesCommand.cs
+++ KfChatDotNetBot/Commands/Kasino/MinesCommand.cs
@@ -238,10 +238,8 @@ public class MinesCommand : ICommand
                     true, autoDeleteAfter: cleanupDelay);
                 return;
             }
-            var msg = await botInstance.SendChatMessageAsync(
-                $"{KasinoMines.ActiveGames[gambler.Id].ToString()}", true);
-            var msgSuccess = await botInstance.WaitForChatMessageAsync(msg, ct: ctx);
-            if (!msgSuccess) throw new InvalidOperationException("Timed out waiting for the message");
+
+            var msg = KasinoMines.ActiveGames[gambler.Id].LastMessage;
             if (pick == 0) //if using coordinates
             {
                 var game = KasinoMines.ActiveGames[gambler.Id];
