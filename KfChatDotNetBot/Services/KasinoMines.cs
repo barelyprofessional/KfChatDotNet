79147640e8137a9a046b7a99bfd53238eefa0bc6
alogindtractor <251821224+A-Log-In-D-Tractor@users.noreply.github.com>
2026-02-10 20:16:26 -0800

    Update KasinoMines.cs (#80)
    fix payouts

--- KfChatDotNetBot/Services/KasinoMines.cs
+++ KfChatDotNetBot/Services/KasinoMines.cs
@@ -301,13 +301,13 @@ public class KasinoMines
 
     public async Task Cashout(KasinoMinesGame game)
     {
-        decimal payout = 0;
+        decimal payout = game.Wager;
         decimal mines = game.Mines;
         decimal size2 = game.Size * game.Size;
         decimal gems = size2 - mines;
         for (int i = 0; i < game.BetsPlaced.Count; i++)
         {
-            payout += game.Wager * ((size2 - i) / gems);
+            payout *= ((size2 - i) / (gems-i));
         }
 
         var newBalance = await Money.NewWagerAsync(game.Creator.Id, game.Wager, payout, WagerGame.Mines);
